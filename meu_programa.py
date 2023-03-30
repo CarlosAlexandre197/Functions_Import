5286040b33cc49484eb3cbc6abd6c90ce7832117
Carlos Alexandre <113312099+CarlosAlexandre197@users.noreply.github.com>
2023-03-30 03:00:50 -0300

    Update meu_programa.py
    Adição de Função.

--- meu_programa.py
+++ meu_programa.py
@@ -71,3 +71,12 @@ def conta_letras(letra_buscada, palavra):
     if letra_atual == letra_buscada:
       contagem += 1
   return contagem
+
+#Função 12
+
+def verifica_prefixo(palavra1, palavra2):
+  tamanho = len(palavra1)
+  for indice in range(tamanho):
+    if palavra1[indice] != palavra2[indice]:
+      return False
+  return True
